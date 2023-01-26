ca2d3aeee448008d6cc1b101e9cc5014b1500505
Daniella Francine P. Velasquez <daniellafrancine.velasquez@gmail.com>
2023-01-27 01:25:22 +0800

    declared a test array

--- bubble-sort.py
+++ bubble-sort.py
@@ -1,7 +1,9 @@
-def bubble_sort(arr):
-    n = len(arr)
+def bubble_sort(array):
+    n = len(array)
  
     for i in range(n):
         for j in range(0, n-i-1):
-            if arr[j] > arr[j+1] :
-                arr[j], arr[j+1] = arr[j+1], arr[j]
+            if array[j] > array[j+1] :
+                array[j], array[j+1] = array[j+1], array[j]
+
+array = [64, 34, 25, 12, 22, 11, 90]
