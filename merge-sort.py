2805de0e22db0e90b04daf713c0874d2035fe93f
Daniella Francine P. Velasquez <daniellafrancine.velasquez@gmail.com>
2023-01-27 18:29:12 +0800

    checks the length of array to be sorted and split checks

--- merge-sort.py
+++ merge-sort.py
@@ -1,5 +1,8 @@
-def merge_sort(nums):
-
+def merge_sort(array):
+    if len(arr) > 1:
+        mid = len(arr)//2
+        L = arr[:mid]
+        R = arr[mid:]
 array = [11, 16, 21, 4, 32, 51, 20, 45, 5, 1]
 merge_sort(array)
 print(array)
