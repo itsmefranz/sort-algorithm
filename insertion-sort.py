57330bc79b1e383512a9b45d70d5b64bbd7ad3b5
Daniella Francine P. Velasquez <daniellafrancine.velasquez@gmail.com>
2023-01-27 18:13:01 +0800

    defined the insertion_sort method and changed the array of numbers

--- insertion-sort.py
+++ insertion-sort.py
@@ -1,3 +1,13 @@
-nums = [53, 82, 9, 13, 98, 73, 18, 61, 16, 62]
+def insertion_sort(array):
+    for i in range(1, len(array)):
+        key = array[i]
+
+        j = i-1
+        while j >=0 and key < array[j] :
+                array[j + 1] = array[j]
+                j -= 1
+        array[j + 1] = key
+
+array = [12, 34, 6, 21, 10, 39, 41, 13, 2, 29]
 insertion_sort(array)
 print(array)
