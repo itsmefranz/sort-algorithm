706ccd17fefec736b57646b9cd0ccabdeb05ca4c
Daniella Francine P. Velasquez <daniellafrancine.velasquez@gmail.com>
2023-01-27 18:33:06 +0800

    declared variables

--- merge-sort.py
+++ merge-sort.py
@@ -1,8 +1,14 @@
 def merge_sort(array):
-    if len(arr) > 1:
+    if len(array) > 1:
         mid = len(arr)//2
-        L = arr[:mid]
-        R = arr[mid:]
+        L = array[:mid]
+        R = array[mid:]
+
+        merge_sort(L)
+        merge_sort(R)
+
+        i = j = k = 0
+
 array = [11, 16, 21, 4, 32, 51, 20, 45, 5, 1]
 merge_sort(array)
 print(array)
