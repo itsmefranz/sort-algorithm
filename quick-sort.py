697e19cec303b437d55a69d76b25b12d6f54455d
Daniella Francine P. Velasquez <daniellafrancine.velasquez@gmail.com>
2023-01-27 19:24:50 +0800

    inserted a calling of elements for pivot

--- quick-sort.py
+++ quick-sort.py
@@ -1,6 +1,8 @@
 def quick_sort(array, low, high):
     if low < high:
         pi = partition(array, low, high)
+        quick_sort(array, low, pi-1)
+        quick_sort(array, pi+1, high)
 
 
 array = [10, 7, 8, 9, 1, 5]
